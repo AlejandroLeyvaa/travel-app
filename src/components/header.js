4c468448a66f06e97c41c895d08a37c377682e6d
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2021-04-12 22:24:26 -0600

    ...

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,38 @@
 import lake from '../assets/images/lake.jpg';
 
-const header = '';
-
-// const header = `
-//   <figure class="hero">
-//     <img src="${lake}" alt="">
-//   </figure>
-
-//   <div class="date">
-//     <h2 class="title">The journey starts now!</h2>
-//     <form class="form" id="form">
-//       <label for="country">
-//         <p class="paragraph">Location</p>
-//         <select id="country">
-//           <option value="Mexico">México</option>
-//           <option value="United States">United States</option>
-//           <option value="United States">United States</option>
-//         </select>
-//       </label>
-//       <label for="date">
-//         <p class="paragraph">Date</p>
-//         <input type="date" id='date' name='date'>
-//       </label>
-//       <label for="guests">
-//         <p class="paragraph">Guests</p>
-//         <input type="number" id='guests' name='guests'>
-//       </label>
-//       <button id="send">Send</button>
-//     </form>
-//   </div>
-// `;
+const header = `
+<figure class="hero">
+<img src="${lake}" alt="">
+</figure>
+<div class="date" id="date">
+<h2 class="title">The journey starts now!</h2>
+<button class="button" id="start">Start journey</button>
+<form class="form" id="form">
+  <div class="close" id="close">
+    <div class="circle">
+      <div class="rec"></div>
+      <div class="rec"></div>
+    </div>
+  </div>
+  <label for="country">
+    <p class="paragraph">Location</p>
+    <select id="country">
+      <option value="Mexico">México</option>
+      <option value="United States">United States</option>
+      <option value="United States">United States</option>
+    </select>
+  </label>
+  <label for="date">
+    <p class="paragraph">Date</p>
+    <input type="date" id='date' name='date' value="2020-05-26">
+  </label>
+  <label for="guests">
+    <p class="paragraph">Guests</p>
+    <input type="number" id='guests' min="2021-04-1" max="2050-08-26" name='guests' value="1">
+  </label>
+  <button class="button" id="send">Send</button>
+</form>
+</div>
+`;
 
 export default header;
